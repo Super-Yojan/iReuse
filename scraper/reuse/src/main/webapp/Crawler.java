ee7599390ccab2d610b2661cbeb4604a98539608
Daniel Jagga <djagga@Daniels-MBP.eduroam.gmu.edu>
2022-03-12 12:35:00 -0500

    Created Spider and Crawler

---
+++ scraper/reuse/src/main/webapp/Crawler.java
@@ -0,0 +1,24 @@
+import javax.swing.text.Document;
+
+public class Crawl
+{
+
+    private List<String> links = new LinkedList<String>();
+    private Document htmlDocument;
+
+    public void crawl() // Give it a URL and it makes an HTTP request for a web page
+    {
+
+    }
+
+    public int searchForWords() // Tries to find a word on the page
+    {
+        return 1;
+    }
+
+    public List<String> getLinks() // Returns a list of all the URLs on the page
+    {
+        List<String> list;
+        return list;
+    }
+}
